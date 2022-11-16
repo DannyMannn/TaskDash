ee683d323264a0e68d1a07004ba033bfa15a2b3c
Mokkuman <droseher@gmail.com>
2022-11-16 14:59:02 -0600

    navBar and home

---
+++ client/html/navBar.php
@@ -0,0 +1,32 @@
+<div class="container-fluid" style="background-color: #e3f2fd;">
+    <div class="container-md">    
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        
+            <a class="navbar-brand" href="#">
+                <img src="" width="30" height="30" alt="Taskdash" />
+            </a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav me-4">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                
+                <li class="nav-item">
+                    <button class="btn btn-outline-success" type="submit">Sign in </button>
+                    <button class="btn btn-outline-success" type="submit">Sign up </button>
+                </li>
+            </ul>
+
+        </nav>
+    </div>   
+</div>
